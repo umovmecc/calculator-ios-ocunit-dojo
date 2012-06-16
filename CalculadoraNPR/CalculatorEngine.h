fb828c44d5e885f7e7ba9d655d8806e9580cfaa2
Otávio Zabaleta <otaviokz@gmail.com>
2012-06-16 16:05:14 -0300

    first commit

---
+++ CalculadoraNPR/CalculatorEngine.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorEngine.h
+//  CalculadoraNPR
+//
+//  Created by Otávio Zabaleta on 6/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorEngine : NSObject
+
+@end
---
+++ CalculadoraNPR/CalculatorEngine.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorEngine.m
+//  CalculadoraNPR
+//
+//  Created by Otávio Zabaleta on 6/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorEngine.h"
+
+@implementation CalculatorEngine
+
+@end
